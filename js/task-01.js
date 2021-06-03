1ce6b82ef27e1534348d3d984a46b6e28f62f49e
Ishidzuka <77970240+Ishidzuka@users.noreply.github.com>
2021-06-03 19:22:21 +0300

    111

--- js/task-01.js
+++ js/task-01.js
@@ -3,6 +3,6 @@ console.log(`В списке ${categorySum.length} категории`);
 
 const categoryText = document.querySelectorAll('.item');
 categoryText.forEach(el => {
-    console.log(`Категория: ${el.firstElementChild.textContent}, Количество элементов :${eL.lastElementChild.children.length}`);
+    console.log(`Категория: ${el.firstElementChild.textContent}, Количество элементов :${el.lastElementChild.children.length}`);
 
 })
